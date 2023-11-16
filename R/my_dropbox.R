0c0c23b41a99bb27e0b64bed4c996cc9f8951f9f
David Holstius <david.holstius@gmail.com>
2023-11-16 09:24:12 -0800

    Update my_dropbox.R

--- R/my_dropbox.R
+++ R/my_dropbox.R
@@ -8,34 +8,30 @@
 #' @param mustWork passed to normalizePath()
 #' @param verbose logical
 #'
-#' @note Looks first for "~/Dropbox (BAAQMD)". Failing that, looks for "~/Dropbox" instead. Throws an error if neither can be found.
+#' @note Looks for the following, in order:
+#' - "BAAQMD Dropbox"
+#' - "Dropbox (BAAQMD)"
+#' - "Dropbox"
+#'
+#' Throws an error if none can be found.
 #'
 #' @seealso [file.path()]
-#'
-#' @examples
-#' csv_path <-
-#'   my_dropbox("BY2015", "Training", "ari", "02-input",
-#'              "BY2015-throughputs-area_sources.csv")
-#'
-#' file.exists(csv_path)
 #'
 #' @export
 my_dropbox <- function (..., mustWork = FALSE, verbose = FALSE) {
 
-  # Prefer this one
-  first_option <- normalizePath(file.path("~", "Dropbox (BAAQMD)"))
+  path_options <- list(
+    file.path("~", "BAAQMD Dropbox"),
+    file.path("~", "Dropbox (BAAQMD)"),
+    file.path("~", "Dropbox"))
 
-  # ... to this one
-  second_option <- normalizePath(file.path("~", "Dropbox"))
-
-  if (dir.exists(first_option)) {
-    result <- file.path(first_option, ...)
-  } else if (dir.exists(second_option)) {
-    result <- file.path(second_option, ...)
-  } else {
-    stop("Can't find a Dropbox directory")
+  for (path in path_options) {
+    abs_path <- normalizePath(path)
+    if (dir.exists(abs_path)) {
+      return(abs_path)
+    }
   }
 
-  return(result)
+  stop("Can't find a Dropbox directory")
 
 }
